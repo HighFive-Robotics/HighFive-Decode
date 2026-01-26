a49973692fce9cdb022fad0bf3c8b4624412383c
CodeYS <123860819+CodeYannSteff@users.noreply.github.com>
2026-01-26 16:40:17 +0200

    Camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/LimeLightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/LimeLightTest.java
@@ -4,9 +4,6 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LimeLightTest {
-    Limelight3A ll;
-    public LimeLightTest(HardwareMap hardwareMap){
-    }
 
 
 }
